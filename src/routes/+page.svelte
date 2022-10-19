4bccf7147d9d755d5fb79e2f23432bdac8459bae
Eduardo Mattje <eduardomattje@gmail.com>
2022-10-19 15:34:10 -0300

    Adicionando lista de ingredientes dinâmica

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,51 @@
 <script lang="ts">
-    import "/src/app.css"
+	import '/src/app.css';
+	import Cabecalho from '$components/Cabecalho.svelte';
+	import MinhaLista from '$lib/components/MinhaLista.svelte';
 </script>
 
 <svelte:head>
-    <title>Alura Cook</title>
+	<title>Alura Cook</title>
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container-principal">
+	<Cabecalho />
+	<div class="estilo-principal">
+		<div class="minha-lista-container">
+			<MinhaLista />
+		</div>
+	</div>
+</div>
 
-<a href="/receitas">Ir para receitas</a>
+<style>
+	.container-principal {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
 
-<img src="/imagens/receitas/panqueca.png" alt="panqueca">
+	.estilo-principal {
+		text-align: center;
+		padding: 0 5vw 3.375rem;
+		flex: 1;
+	}
+
+	.minha-lista-container {
+		margin-bottom: 2rem;
+	}
+
+	.divisoria {
+		width: 40vw;
+		height: 2px;
+		background-color: var(--verde);
+		margin: 0 auto;
+	}
+
+	.info {
+		margin-bottom: 3.375rem;
+	}
+
+	.info > p {
+		line-height: 2rem;
+	}
+</style>
